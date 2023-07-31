31434e96ea96c58c75b50e1ead892e3993991dac
needl3 <57314527+needl3@users.noreply.github.com>
2023-07-31 21:40:44 +0545

    Add 8 bit color support

--- VideoToAscii/src/main.rs
+++ VideoToAscii/src/main.rs
@@ -9,17 +9,28 @@ const PIX_BITS: usize = 4;
 
 fn get_ascii(intensity: u8) -> &'static str {
     let ascii = [
-        " ", ".", ",", "_", "-", "^", "*", "=", "+", "i", "r", "v", "a", "g", "(", "C", "A", "B",
-        "#",
+        " ", ".", "~", "+", "=", "r", "c", "a", "g", "x", "C", "A", "B", "M", "%", "$", "#",
     ];
-    let map_left = 1;
-    let index = intensity
-        / ((255 / (ascii.len() as u8 - map_left)) + (255 % (ascii.len() as u8 - map_left)));
+    let index = intensity / ((255 / ascii.len() as u8) + (255 % ascii.len() as u8));
 
     return ascii[index as usize];
 }
 
-fn draw_ascii_image(image: &DynamicImage) {
+fn calculate_intensity(pix: (&u8, &u8, &u8, &u8)) -> u8 {
+    // This is avegaging technique
+    // return *pix.0/3 + *pix.1/3 + *pix.2/3;
+    //
+
+    //
+    // This considers physical brightness of colors
+    //
+    let gottem = *pix.0 as f32 * *pix.0 as f32 * 0.241
+        + *pix.1 as f32 * *pix.1 as f32 * 0.691
+        + *pix.2 as f32 * *pix.2 as f32 * 0.068;
+    return gottem.sqrt() as u8;
+}
+
+fn draw_ascii_image(image: &DynamicImage, color: i8) {
     let (width, height) = image.dimensions();
 
     let scale = height / 200;
@@ -28,31 +39,49 @@ fn draw_ascii_image(image: &DynamicImage) {
         for x in 0..width {
             if y % (scale * 2) == 0 && x % (scale * 2) == 0 {
                 let pix = image.get_pixel(x, y);
-                let intensity = calculate_intensity(&(pix[0], pix[1], pix[2], pix[3]));
-                print!("{}", get_ascii(intensity));
+                let intensity = calculate_intensity((&pix[0], &pix[1], &pix[2], &pix[3]));
+
+                if color > 0 {
+                    print!(
+                        "\x1B[38;2;{};{};{};48;2;{};{};{}m{}",
+                        pix[0],
+                        pix[1],
+                        pix[2],
+                        pix[0],
+                        pix[1],
+                        pix[2],
+                        get_ascii(intensity)
+                    );
+                } else if color > -1 {
+                    print!(
+                        "\x1B[38;2;{};{};{}m{}",
+                        pix[0],
+                        pix[1],
+                        pix[2],
+                        get_ascii(intensity)
+                    );
+                } else {
+                    print!("{}", get_ascii(intensity));
+                }
             }
         }
+        print!("\x1B[38;2;255;255;255;48;2;0;0;0m");
+
         if y % (scale * 2) == 0 {
             println!();
         }
     }
 }
 
-fn calculate_intensity(pix: &(u8, u8, u8, u8)) -> u8 {
-    // This is avegaging technique
-    // return *pix.0/3 + *pix.1/3 + *pix.2/3;
-    //
-
-    //
-    // This considers physical brightness of colors
-    //
-    let gottem = pix.0 as f32 * pix.0 as f32 * 0.241
-        + pix.1 as f32 * pix.1 as f32 * 0.691
-        + pix.2 as f32 * pix.2 as f32 * 0.068;
-    return gottem.sqrt() as u8;
+fn use_image(source: &String, color: i8) {
+    let image = image::open(source).unwrap();
+    draw_ascii_image(&image, color);
 }
 
-fn draw_ascii_video(frame: &Frame) {
+fn draw_ascii_video(frame: &Frame, color: i8) {
+    //
+    // color: -1 = no color, 0 = text only, 1 = text and background color
+    //
     let (_, width) = frame.resolution;
 
     let scale = 2;
@@ -66,22 +95,45 @@ fn draw_ascii_video(frame: &Frame) {
             y = pixel_index as u32 / width;
         }
         if y % (scale * 2) == 0 && x % (scale / 2) == 0 {
-            let intensity = calculate_intensity(&(
-                frame[index],
-                frame[index + 1],
-                frame[index + 2],
-                frame[index + 3],
+            let intensity = calculate_intensity((
+                &frame[index],
+                &frame[index + 1],
+                &frame[index + 2],
+                &frame[index + 3],
             ));
-            print!("{}", get_ascii(intensity));
+
+            if color > 0 {
+                print!(
+                    "\x1B[38;2;{};{};{};48;2;{};{};{}m{}",
+                    frame[index],
+                    frame[index + 1],
+                    frame[index + 2],
+                    frame[index],
+                    frame[index + 1],
+                    frame[index + 2],
+                    get_ascii(intensity)
+                );
+            } else if color > -1 {
+                print!(
+                    "\x1B[38;2;{};{};{}m{}",
+                    frame[index],
+                    frame[index + 1],
+                    frame[index + 2],
+                    get_ascii(intensity)
+                );
+            } else {
+                print!("{}", get_ascii(intensity));
+            }
+            if x == width - 1 {
+                print!("\x1B[38;2;255;255;255;48;2;0;0;0m");
+                println!();
+            }
         }
         index = index + 4;
-        if y % (scale * 2) == 0 && x == width - 1 {
-            println!();
-        }
     }
 }
 
-fn use_camera(source: &String) {
+fn use_camera(source: &String, color: i8) {
     let camera_result = Camera::new(source);
     let mut camera = match camera_result {
         Ok(camera) => camera,
@@ -110,14 +162,9 @@ fn use_camera(source: &String) {
             }
         };
         print!("\x1B[2J\x1B[1;1H");
-        draw_ascii_video(&frame);
-        thread::sleep(Duration::from_millis(34));
+        draw_ascii_video(&frame, color);
+        thread::sleep(Duration::from_millis(33));
     }
-}
-
-fn use_image(source: &String) {
-    let image = image::open(source).unwrap();
-    draw_ascii_image(&image);
 }
 
 fn help() {
@@ -125,6 +172,11 @@ fn help() {
     println!("Options:");
     println!("\t--video /dev/video0");
     println!("\t--image /path/to/image");
+    println!("\t\t--color <options>");
+    println!("Color options:");
+    println!("No options: Only use foreground colors");
+    println!("bgcolor : Use background color");
+    println!("Note: Omit --color to not use colored rendering");
 }
 
 fn main() {
@@ -137,8 +189,38 @@ fn main() {
         3 => {
             let option = &args[1];
             match &option[..] {
-                "--video" => use_camera(&args[2]),
-                "--image" => use_image(&args[2]),
+                "--video" => use_camera(&args[2], -1),
+                "--image" => use_image(&args[2], -1),
+                _ => help(),
+            }
+        }
+        4 => {
+            if args[3] != "--color" {
+                help();
+                return;
+            }
+            println!("\n\n NOTE: Colored Rendering will be choppy");
+            thread::sleep(Duration::from_millis(1000));
+
+            let option = &args[1];
+            match &option[..] {
+                "--video" => use_camera(&args[2], 0),
+                "--image" => use_image(&args[2], 0),
+                _ => help(),
+            }
+        }
+        5 => {
+            if args[3] != "--color" {
+                help();
+                return;
+            }
+            println!("\n\nNOTE: Colored Rendering will be the choppiest");
+            thread::sleep(Duration::from_millis(1000));
+ 
+            let option = &args[1];
+            match &option[..] {
+                "--video" => use_camera(&args[2], 1),
+                "--image" => use_image(&args[2], 1),
                 _ => help(),
             }
         }
